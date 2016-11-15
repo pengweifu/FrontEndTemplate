adf5b0ad86d0adb0d44c8d13bc44eaa4685e5954
pengweifu <987528260@qq.com>
2016-11-15 20:57:21 +0800

    update

--- js/data.js
+++ js/data.js
@@ -15,11 +15,13 @@ $(function(){
       dataType: 'json'
     }).done(function(data, status, jqXHR) {
       var tmp='<li class="swiper-slide"><a href="{$field.url}" title="{$field.title}"><span class="thumb"><img src="{$field.img}" alt="{$field.title}"></span><span class="caption">{$field.title}</span></a></li>';
-      for (var ii = 0; ii < $('.swiper-wrapper:not(.manual)').length; ii++) {
-        var root=$('.swiper-wrapper:not(.manual)').eq(ii).parent().parent().attr('class');
+      for (var ii = 0; ii < $('.swiper-wrapper').length; ii++) {
+        var root=$('.swiper-wrapper').eq(ii).parent().parent().attr('class');
         root='.'+root+' ';
-        for (var i in data.list) {
-          $(root+'.swiper-wrapper').append(tmp.replace(/\{\$field\.url\}/g,data.list[i].url).replace(/\{\$field\.title\}/g,data.list[i].title).replace(/\{\$field\.img\}/g,data.list[i].img));
+        if ($(root+' .swiper-wrapper li').length==0) {
+          for (var i in data.list) {
+            $(root+' .swiper-wrapper').append(tmp.replace(/\{\$field\.url\}/g,data.list[i].url).replace(/\{\$field\.title\}/g,data.list[i].title).replace(/\{\$field\.img\}/g,data.list[i].img));
+          }
         }
         calcSwiper(root);
       }
@@ -42,8 +44,12 @@ $(function(){
       dataType: 'json'
     }).done(function(data, status, jqXHR) {
       var tmp='<li><a href="{$field.url}" title="{$field.title}"><span class="thumb"><img src="{$field.img}" alt="{$field.title}"></span><span class="caption"><i class="pull-right">{$field.date}</i>{$field.title}</span></a></li>';
-      for (var i in data.list) {
-        $('.m-list').append(tmp.replace(/\{\$field\.url\}/g,data.list[i].url).replace(/\{\$field\.title\}/g,data.list[i].title).replace(/\{\$field\.img\}/g,data.list[i].img).replace(/\{\$field\.date\}/g,data.list[i].date));
+      for (var ii = 0; ii < $('.m-list').length; ii++) {
+        if ($('.m-list').eq(ii).children('li').length==0) {
+          for (var i in data.list) {
+            $('.m-list').eq(ii).append(tmp.replace(/\{\$field\.url\}/g,data.list[i].url).replace(/\{\$field\.title\}/g,data.list[i].title).replace(/\{\$field\.img\}/g,data.list[i].img).replace(/\{\$field\.date\}/g,data.list[i].date));
+          }
+        }
       }
     }).fail(function(status, jqXHR){console.log(status);});
   }
